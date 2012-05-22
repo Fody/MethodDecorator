5e46f64ee4107d66cea8d8363ae335c34f5ff076
Matt Ellis <m.t.ellis@gmail.com>
2012-05-22 19:29:44 +0100

    Tests for generic methods

--- MethodDecorator.Fody.Tests/When_decorating_generic_methods.cs
+++ MethodDecorator.Fody.Tests/When_decorating_generic_methods.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 using Xunit;
 
 namespace MethodDecorator.Fody.Tests
 {
-    public class When_decorating_generic_types : IUseFixture<DecoratedSimpleTest>
+    public class When_decorating_generic_methods : IUseFixture<DecoratedSimpleTest>
     {
         private Assembly assembly;
         private dynamic testClass;
@@ -14,18 +14,18 @@ namespace MethodDecorator.Fody.Tests
         public void Should_capture_on_entry_and_exit()
         {
             const string expected = "Hello world";
-            var value = testClass.GetValue(expected);
+            var value = testClass.GetValue<string>(expected);
 
             Assert.Equal(expected, value);
 
-            Assert.Contains("OnEntry: SimpleTest.GenericType`1.GetValue", testMessages.Messages);
-            Assert.Contains("OnExit: SimpleTest.GenericType`1.GetValue", testMessages.Messages);
+            Assert.Contains("OnEntry: SimpleTest.GenericMethod.GetValue", testMessages.Messages);
+            Assert.Contains("OnExit: SimpleTest.GenericMethod.GetValue", testMessages.Messages);
         }
 
         public void SetFixture(DecoratedSimpleTest data)
         {
             assembly = data.Assembly;
-            testClass = assembly.GetInstance("SimpleTest.GenericType`1[[System.String, mscorlib]]");
+            testClass = assembly.GetInstance("SimpleTest.GenericMethod");
             testMessages = assembly.GetStaticInstance("SimpleTest.TestMessages");
             testMessages.Clear();
         }
---
+++ TestAssemblies/SimpleTest/GenericMethod.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleTest
+{
+    public class GenericMethod
+    {
+        [Interceptor] 
+        public T GetValue<T>(T value)
+        {
+            TestMessages.Record(string.Format("GenericMethod.GetValue<{0}> - body", typeof(T)));
+
+            return value;
+        }
+    }
+}
