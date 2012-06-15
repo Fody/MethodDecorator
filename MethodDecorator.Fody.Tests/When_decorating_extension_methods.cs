7662752a9b58643390dd20562b872b9aff6907ad
Matt Ellis <m.t.ellis@gmail.com>
2012-06-15 15:56:15 +0100

    Added tests for decorating extension methods

---
+++ MethodDecorator.Fody.Tests/When_decorating_extension_methods.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+using Xunit;
+
+namespace MethodDecorator.Fody.Tests
+{
+    public class When_decorating_extension_methods : IUseFixture<DecoratedSimpleTest>
+    {
+        private Assembly assembly;
+        private dynamic testClass;
+        private dynamic testMessages;
+
+        [Fact]
+        public void Should_intercept_extension_method()
+        {
+            var value = testClass.ReturnsString();
+
+            Assert.Equal(3, testMessages.Messages.Count);
+            Assert.Contains("OnEntry: SimpleTest.StringExtensions.ToTitleCase", testMessages.Messages);
+            Assert.Contains("ToTitleCase: In extension method", testMessages.Messages);
+            Assert.Contains("OnExit: SimpleTest.StringExtensions.ToTitleCase", testMessages.Messages);
+            Assert.Equal("Hello World", value);
+        }
+
+        public void SetFixture(DecoratedSimpleTest data)
+        {
+            assembly = data.Assembly;
+            testClass = assembly.GetInstance("SimpleTest.InterceptingExtensionMethods");
+            testMessages = assembly.GetStaticInstance("SimpleTest.TestMessages");
+            testMessages.Clear();
+        } 
+    }
+}
---
+++ TestAssemblies/SimpleTest/InterceptingExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace SimpleTest
+{
+    public class InterceptingExtensionMethods
+    {
+        public string ReturnsString()
+        {
+            return "hello world".ToTitleCase();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        [Interceptor]
+        public static string ToTitleCase(this string value)
+        {
+            TestMessages.Record("ToTitleCase: In extension method");
+            return new CultureInfo("en-GB", false).TextInfo.ToTitleCase(value);
+        }
+    }
+}
