00af604875406c9323e155d9f2acbdb44b315ac9
Simon Cropp <simon.cropp@gmail.com>
2013-03-30 12:17:19 +1100

    add sample

---
+++ TestAssemblies/SimpleTest/Sample.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace SimpleTest
+{
+    public class Sample
+    {
+        [Interceptor]
+        public void Method()
+        {
+            Debug.WriteLine("Your Code");
+        }
+    }
+}
