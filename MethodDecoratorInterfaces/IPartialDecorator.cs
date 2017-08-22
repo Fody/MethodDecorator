634b1ad915a957b70fe8c3df20bc0aaa64fe1a73
Sergey Stronin <s.stronin@gmail.com>
2017-08-22 19:16:37 +0300

    Partial decoration - interfaces

---
+++ MethodDecoratorInterfaces/IPartialDecorator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MethodDecorator.Fody.Interfaces
+{
+    public interface IPartialDecoratorInit1
+    {
+        void Init(MethodBase iMethod);
+    }
+    public interface IPartialDecoratorInit2
+    {
+        void Init(object iInstance,MethodBase iMethod);
+    }
+    public interface IPartialDecoratorInit3
+    {
+        void Init(object iInstance, MethodBase iMethod, object[] iParameters);
+    }
+    public interface IPartialDecoratorEntry
+    {
+        void OnEntry();
+    }
+    public interface IPartialDecoratorExit
+    {
+        void OnExit();
+    }
+    public interface IPartialDecoratorExit1
+    {
+        void OnExit(object iRetval);
+    }
+    public interface IPartialDecoratorException
+    {
+        void OnException(Exception iException);
+    }
+    interface IPartialDecoratorContinuation
+    {
+        void OnTaskContinuation(Task task);
+    }
+}
