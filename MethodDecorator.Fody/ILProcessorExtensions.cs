459710aa2feb34d1d346e24c2b767c95e168c495
Simon Cropp <simon.cropp@gmail.com>
2018-08-05 17:56:45 +1000

    cleanup

--- MethodDecorator.Fody/ILProcessorExtensions.cs
+++ MethodDecorator.Fody/ILProcessorExtensions.cs
@@ -1,19 +1,22 @@
 using System.Collections.Generic;
-
 using Mono.Cecil.Cil;
 
-namespace MethodDecorator.Fody {
-    public static class IlProcessorExtensions {
-        public static void InsertBefore(this ILProcessor processor, Instruction target, IEnumerable<Instruction> instructions) {
-            foreach (var instruction in instructions)
-                processor.InsertBefore(target, instruction);
+public static class IlProcessorExtensions
+{
+    public static void InsertBefore(this ILProcessor processor, Instruction target, IEnumerable<Instruction> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            processor.InsertBefore(target, instruction);
         }
+    }
 
-        public static void InsertAfter(this ILProcessor processor, Instruction target, IEnumerable<Instruction> instructions) {
-            foreach (var instruction in instructions) {
-                processor.InsertAfter(target, instruction);
-                target = instruction;
-            }
+    public static void InsertAfter(this ILProcessor processor, Instruction target, IEnumerable<Instruction> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            processor.InsertAfter(target, instruction);
+            target = instruction;
         }
     }
 }
--- MethodDecorator.Fody/TypeReferenceExtensions.cs
+++ MethodDecorator.Fody/TypeReferenceExtensions.cs
@@ -1,42 +1,50 @@
 using System;
 using System.Linq;
-
+using MethodDecorator.Fody;
 using Mono.Cecil;
 
-namespace MethodDecorator.Fody {
-    public static class TypeReferenceExtensions {
-
-        public static bool Implements(this TypeDefinition typeDefinition, Type type) {
-            if (type.IsInterface == false) {
-                throw new InvalidOperationException("The <type> argument (" + type.Name + ") must be an Interface type.");
-            }
-
-            var referenceFinder = new ReferenceFinder(typeDefinition.Module);
-            var baseTypeDefinition = referenceFinder.GetTypeReference(type);
-
-            return typeDefinition.Implements(baseTypeDefinition);
+public static class TypeReferenceExtensions
+{
+    public static bool Implements(this TypeDefinition typeDefinition, Type type)
+    {
+        if (type.IsInterface == false)
+        {
+            throw new InvalidOperationException($"The <type> argument ({type.Name}) must be an Interface type.");
         }
 
-        public static bool Implements(this TypeDefinition typeDefinition, TypeReference interfaceTypeReference) {
-            while (typeDefinition != null && typeDefinition.BaseType != null) {
-                if (typeDefinition.Interfaces != null && typeDefinition.Interfaces.Any(i => i.InterfaceType.FullName == interfaceTypeReference.FullName))
-                    return true;
+        var referenceFinder = new ReferenceFinder(typeDefinition.Module);
+        var baseTypeDefinition = referenceFinder.GetTypeReference(type);
 
-                typeDefinition = typeDefinition.BaseType.Resolve();
+        return typeDefinition.Implements(baseTypeDefinition);
+    }
+
+    public static bool Implements(this TypeDefinition typeDefinition, TypeReference interfaceTypeReference)
+    {
+        while (typeDefinition?.BaseType != null)
+        {
+            if (typeDefinition.Interfaces != null && typeDefinition.Interfaces.Any(i => i.InterfaceType.FullName == interfaceTypeReference.FullName))
+            {
+                return true;
             }
 
-            return false;
+            typeDefinition = typeDefinition.BaseType.Resolve();
         }
 
-        public static bool DerivesFrom(this TypeReference typeReference, TypeReference expectedBaseTypeReference) {
-            while (typeReference != null) {
-                if (typeReference.FullName == expectedBaseTypeReference.FullName)
-                    return true;
+        return false;
+    }
 
-                typeReference = typeReference.Resolve().BaseType;
+    public static bool DerivesFrom(this TypeReference typeReference, TypeReference expectedBaseTypeReference)
+    {
+        while (typeReference != null)
+        {
+            if (typeReference.FullName == expectedBaseTypeReference.FullName)
+            {
+                return true;
             }
 
-            return false;
+            typeReference = typeReference.Resolve().BaseType;
         }
+
+        return false;
     }
 }
